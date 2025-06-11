f5296c31bc06c4d3719aa8da63e9bdbe3d87abc8
Chanyeol Park <chanyeol@si-analytics.ai>
2025-06-11 15:44:28 +0900

    기능개발 코드 수정

--- src/main/kotlin/코딩_테스트_합격자_되기/큐/기능개발.kt
+++ src/main/kotlin/코딩_테스트_합격자_되기/큐/기능개발.kt
@@ -4,7 +4,7 @@ class Solution11 {
     // 뒤에 있는 기능은 앞에 있는 기능보다 먼저 개발될 수는 있으나, 앞에 있는 기능이 배포되어야 뒤에 있는 기능도 배포가 가능하다.
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
         val workDays = ArrayDeque<Int>()
-        var answer = intArrayOf()
+        var answer = mutableListOf<Int>()
         for (i in progresses.indices) {
             val q = (100 - progresses[i]) / speeds[i]
             val r = (100 - progresses[i]) % speeds[i]
@@ -18,10 +18,10 @@ class Solution11 {
                 count++
                 workDays.removeFirst()
             }
-            answer = answer.plus(count)
+            answer.add(count)
         }
 
-        return answer
+        return answer.toIntArray()
     }
 }
 
@@ -30,19 +30,11 @@ fun main() {
     // 작업 일정 7일에 두작업 배포 가능
     // 작업 일정 9일에 한 작업 배포 가능
     // 결과 (2,1)
-    val result = Solution11().solution(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5))
-    for (i in result) {
-        println(i)
-    }
-//    println(Solution11().solution(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5)))
+    Solution11().solution(intArrayOf(93, 30, 55), intArrayOf(1, 30, 5))
     // 작업 일정 5,10,1,1,20,1
     // 작업 일정 5일 뒤에 한개 작업 배포 가능
     // 작업 일정 10 일 뒤에 3개 작업 배포 가능
     // 작업 일정 20일 뒤에 2개 작업 배포 가능
     // 결과 (1,3,2)
-    val result2 = Solution11().solution(intArrayOf(95, 90, 99, 99, 80, 99), intArrayOf(1, 1, 1, 1, 1, 1))
-    for (i in result2) {
-        println(i)
-    }
-//    println(Solution11().solution(intArrayOf(95, 90, 99, 99, 80, 99), intArrayOf(1, 1, 1, 1, 1, 1)))
+    Solution11().solution(intArrayOf(95, 90, 99, 99, 80, 99), intArrayOf(1, 1, 1, 1, 1, 1))
 }
